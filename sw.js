eff088e77e84665ec8d2c73d1e7a841a41c3ccda
study progress <aaditya2726@gmail.com>
2026-07-12 11:40:11 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE = 'studyx-v2-july';
+const ASSETS = [
+  './',
+  './index.html',
+  './logo.png',
+  'https://fonts.googleapis.com/css2?family=Rajdhani:wght@400;600;700&family=Space+Mono:wght@400;700&display=swap'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS).catch(() => {}))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
